5e93e01187ec96746134941f32c1ad53f845d6ab
017lead <141877618+017lead@users.noreply.github.com>
2024-06-20 19:18:37 -0500

    New

--- arfollow.cs
+++ arfollow.cs
@@ -1,20 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class arfollow : MonoBehaviour
+public class ARFollow : MonoBehaviour
 {
     public GameObject Player;
     public float positionSmoothSpeed = 0.125f;
     public float rotationSmoothSpeed = 0.125f;
 
-    // Update is called once per frame
+    private bool isEnabled = true;
+
     void Update()
     {
-        // Smoothly interpolate the position
-        Player.transform.position = Vector3.Lerp(Player.transform.position, transform.position, positionSmoothSpeed);
+        if (isEnabled)
+        {
+            // Smoothly interpolate the position
+            Player.transform.position = Vector3.Lerp(Player.transform.position, transform.position, positionSmoothSpeed);
+            // Smoothly interpolate the rotation
+            Player.transform.rotation = Quaternion.Slerp(Player.transform.rotation, transform.rotation, rotationSmoothSpeed);
+        }
+    }
 
-        // Smoothly interpolate the rotation
-        Player.transform.rotation = Quaternion.Slerp(Player.transform.rotation, transform.rotation, rotationSmoothSpeed);
+    public void ToggleScript(bool enable)
+    {
+        isEnabled = enable;
     }
 }
